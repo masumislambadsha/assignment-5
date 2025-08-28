445949eb3e244f2292bf844168ae9a6f6ab5045d
Masum Islam Badsha <nkfhhdndjdh@gmail.com>
2025-08-29 01:19:38 +0600

    safe push

--- script.js
+++ script.js
@@ -9,24 +9,26 @@ function getInnerTextValue(id) {
 }
 
 const historyData = [];
-
-
-
-
 // function to increase favNum
-const favBtns = document.querySelectorAll('.fav-btn')
-for (const favBtn of favBtns) {
-      favBtn.addEventListener("click", function () {
+function favCount(id) {
+  document.getElementById(id).addEventListener("click", function () {
     let count = getInnerTextValue("fav-count");
     let newCount = count + 1 + `  ❤️`;
     document.getElementById("fav-count").innerText = newCount;
   });
 }
+favCount("fav-btn2");
+favCount("fav-btn1");
+favCount("fav-btn3");
+favCount("fav-btn4");
+favCount("fav-btn5");
+favCount("fav-btn6");
+favCount("fav-btn7");
+favCount("fav-btn8");
+favCount("fav-btn9");
 
 // function to show alert from call
-const callBtns = document.querySelectorAll('.call')
-for (const callBtn of callBtns) {
-  callBtn.addEventListener("click", function () {
+document.getElementById("call1").addEventListener("click", function () {
   let coin = getInnerTextValue("coin");
   if (coin <= 0) {
     alert("Insufficient Coin 🪙");
@@ -43,7 +45,144 @@ for (const callBtn of callBtns) {
   historyData.push(data);
   updateHistory();
 });
-}
+
+document.getElementById("call2").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Police 999...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Police Helpline Number",
+    number: "999",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call3").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Fire Service 999...");
+
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Fire Service Number",
+    number: "999",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call4").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Ambulance 1994-999999...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Ambulance Number",
+    number: "1994-999999",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call5").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Women & Child Helpline 109...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Women & Child Helpline",
+    number: "109",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call6").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Anti-Corruption 106...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Anti-Corruption Helpline",
+    number: "106",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call7").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Electricity Outage 16216...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Electricity Helpline",
+    number: "16216",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call8").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Brac 16445...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Brac Helpline",
+    number: "16445",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
+document.getElementById("call9").addEventListener("click", function () {
+  let coin = getInnerTextValue("coin");
+  if (coin <= 0) {
+    alert("Insufficient Coin 🪙");
+    return;
+  }
+  alert("📞Calling Bangladesh Railway 163...");
+  let newCoinValue = coin - 20;
+  document.getElementById("coin").innerText = newCoinValue + ` 🪙`;
+  const data = {
+    name: "Bangaldesh Railway Helpline",
+    number: "163",
+    date: new Date().toLocaleTimeString(),
+  };
+  historyData.push(data);
+  updateHistory();
+});
 
 // update history sectio
 function updateHistory() {
@@ -75,14 +214,29 @@ document.getElementById("clearBtn").addEventListener("click", function () {
   document.getElementById("history-container").innerText = "";
 });
 
+//copy count increase
 
-// copy number + copy count increase
-function copyNumber(btnId, numberText) {
-  document.getElementById(btnId).addEventListener("click", function () {
+function copyCount(id) {
+  document.getElementById(id).addEventListener("click", function () {
     alert("copy count increased");
     let count = parseInt(document.getElementById("copyCount").innerText);
     let newCount = count + 1 + "  copy";
     document.getElementById("copyCount").innerText = newCount;
+  });
+}
+copyCount("copyBtn1");
+copyCount("copyBtn2");
+copyCount("copyBtn3");
+copyCount("copyBtn4");
+copyCount("copyBtn5");
+copyCount("copyBtn6");
+copyCount("copyBtn7");
+copyCount("copyBtn8")
+copyCount("copyBtn9");
+
+// copy number
+function copyNumber(btnId, numberText) {
+  document.getElementById(btnId).addEventListener("click", function () {
     navigator.clipboard.writeText(numberText).then(function () {
 
     })
